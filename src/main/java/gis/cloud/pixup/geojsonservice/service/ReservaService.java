4b564bb967ebdbd7d01d17f94be3751dde25335c
JoseLuisVB001 <117409168+JoseLuisVB001@users.noreply.github.com>
2023-10-06 14:14:00 -0600

    Delete src/main directory

--- src/main/java/gis/cloud/pixup/geojsonservice/service/ReservaService.java
+++
@@ -1,9 +0,0 @@
-package gis.cloud.pixup.geojsonservice.service;
-
-import gis.cloud.pixup.geojsonservice.domain.Reserva;
-
-public interface ReservaService {
-	
-	Reserva actualizarReserva(String id, Reserva reserva);
-
-}
